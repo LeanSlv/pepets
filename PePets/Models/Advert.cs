9e8634739eebab9b30c91b0b1b599978910b6242
Mihail Solovev <youoliver@mail.ru>
2020-03-09 20:37:45 +0400

    Update structure of images property in Advert model

--- PePets/Models/Advert.cs
+++ PePets/Models/Advert.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,17 @@ namespace PePets.Models
 {
     public class Advert
     {
+        internal string _images { get; set; }
+
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public ICollection<Image> Images { get; set; }
+
+        [NotMapped]
+        public string[] Images { 
+            get { return _images == null ? null : JsonConvert.DeserializeObject<string[]>(_images); }
+            set { _images = JsonConvert.SerializeObject(value); } 
+        }
         public int Cost { get; set; }
         public Pet PetDecription { get; set; }
         public Guid UserId { get; set; }
